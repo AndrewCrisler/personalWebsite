8fbb8bb63277c7ca5d3b8faa620c97c8444c66e4
AndrewCrisler <andrew.t.c000@gmail.com>
2021-01-17 15:48:57 -0600

    Reworked the css to create a semi-dynamicly changing design that is lined up. Also, small changes to the js and html

--- mainScript.js
+++ mainScript.js
@@ -6,6 +6,12 @@ var header = document.getElementById("header");
 
 //get the about section
 var aboutSection = document.getElementById("aboutSection");
+
+//get the root in css
+let root = document.documentElement;
+
+//get the tester text
+var tester = document.getElementById("tester");
 //navbar goes to sticky when it passes the header, could not find a way to get the headers height
 var sticky = 75;
 
@@ -27,4 +33,22 @@ function stickyHeader(){
     }
 }
 
+//initializing code
+let screenSize = document.documentElement.clientWidth;
+tester.innerHTML = screenSize;
+
+root.style.setProperty('--widthFull', (screenSize) + "px");
+//root.style.setProperty('--width10', (screenSize * 0.1) + "px");
+
+
+//ongoing functions
 window.onscroll = function() {stickyHeader()};
+
+window.onresize = function() {
+    let screenSize = document.documentElement.clientWidth;
+    tester.innerHTML = screenSize;
+
+    root.style.setProperty('--widthFull', (screenSize) + "px");
+    //root.style.setProperty('--width10', (screenSize * 0.1) + "px");
+}   
+
